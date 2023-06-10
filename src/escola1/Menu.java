17df294c810e047d51e31aca93330f7910f2faed
Matheus alcantara <alvantaramatheus@gmail.com>
2023-06-09 21:08:29 -0300

    testando git

--- src/escola1/Menu.java
+++ src/escola1/Menu.java
@@ -10,6 +10,7 @@ public class Menu {
 		System.out.println("digite 1 para cadastrar aluno ou 2 para professor ou 3 para funcionario:");
 		int escolha = scanner.nextInt();
 		if (escolha == 1) {
+			System.out.println("testando o git!");
 
 		}
 	}
